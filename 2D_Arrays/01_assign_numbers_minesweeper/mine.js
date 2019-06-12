b31e4f8c979fbc0e3f85a431435dd0da51bd0910
charles161 <fortloop.charles@gmail.com>
2019-06-13 05:11:19 +0530

    added own code for minesweeper

---
+++ 2D_Arrays/01_assign_numbers_minesweeper/mine.js
@@ -0,0 +1,52 @@
+function checkaround(cr, cl, lr, lc, arr) {
+ let count = 0;
+ if ((cr - 1) >= 0) {
+  if (arr[cr - 1][cl] == -1) count++;
+ }
+ if ((cl - 1) >= 0) {
+  if (arr[cr][cl - 1] == -1) count++;
+ }
+ if ((cr + 1) < lr) {
+  if (arr[cr + 1][cl] == -1) count++;
+ }
+ if ((cl + 1) < lc) {
+  if (arr[cr][cl + 1] == -1) count++;
+ }
+ if ((cr + 1) < lr && (cl + 1) < lc)
+  if (arr[cr + 1][cl + 1] == -1) count++;
+ if ((cr - 1) >= 0 && (cl - 1) >= 0)
+  if (arr[cr - 1][cl - 1] == -1) count++;
+ if ((cr + 1) < lr && (cl - 1) >= 0)
+  if (arr[cr + 1][cl - 1] == -1) count++;
+ if ((cr - 1) >= 0 && (cl + 1) < lc)
+  if (arr[cr - 1][cl + 1] == -1) count++;
+
+
+ return count;
+}
+
+function mine_sweeper(bombs, nr, nc) {
+ let arr = new Array(nr), i, j;
+ console.log(bombs)
+ for (i = 0; i < nr; i++) {
+
+  arr[i] = []
+
+ }
+ for (i = 0; i < bombs.length; i++) {
+  let t = bombs[i];
+  arr[t[0]][t[1]] = -1;
+ }
+ for (i = 0; i < nr; i++) {
+  for (j = 0; j < nc; j++) {
+   if (arr[i][j] != -1)
+    arr[i][j] = checkaround(i, j, nr, nc, arr);
+
+  }
+ }
+
+ return arr;
+}
+
+console.log(mine_sweeper([[0, 0], [0, 1]], 3, 4))
+
