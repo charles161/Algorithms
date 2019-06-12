179b8fc6d302300e13d0bb3f450631bcd5480f46
charles161 <fortloop.charles@gmail.com>
2019-06-13 04:08:25 +0530

    added org code for one away

--- Strings/02_one_away/mine.js
+++ Strings/02_one_away/mine.js
@@ -5,6 +5,7 @@ function one_away(s1, s2) {
         let flag = 0,
             count = 0;
         while (p1 < s1.length && p2 < s2.length) {
+            if (count > 1) return false;
             if (s1[p1] == s2[p2]) {
                 p1++;
                 p2++;
@@ -14,6 +15,7 @@ function one_away(s1, s2) {
                 //     return false;
                 // }
                 count++;
+
                 //flag = 1;
                 if (s1.length == s2.length) {
                     p1++;
